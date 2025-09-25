ab518d907cdf90fcdc4a3b9f45b97b9e2cda02b2
Lufford <146119753+Lufford@users.noreply.github.com>
2025-09-24 20:09:03 -0400

    placeholder assets
    added placeholder player (no components) and placeholder enemy

---
+++ FlyTrapSnackAttack/Assets/Scripts/Enemy/Projectiles/Spear.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Spear : MonoBehaviour
+{
+    [SerializeField] public Transform target;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
